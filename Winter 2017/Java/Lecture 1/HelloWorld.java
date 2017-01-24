5958aa61110f8fbd7ca22f6796aef4de1d05b6d5
Marc Cataford <mcataford@users.noreply.github.com>
2017-01-24 14:48:54 -0500

    Added example HelloWorld.
    First program + comments.

---
+++ Winter 2017/Java/Lecture 1/HelloWorld.java
@@ -0,0 +1,27 @@
+/*
+* Hello, world!
+* File: HelloWorld.java
+* Author: Marc Cataford
+* 
+* A simple program that prints 'Hello, world!' to the screen.
+*/
+
+/*
+* This is a class, it is the top level container for our code.
+* The name of the class must match the name of the file it is in,
+* hence why our file is called 'HelloWorld.java'.
+*/
+public class HelloWorld {
+  /*
+  * This is a method. It represents a function, or set or instructions, that can be executed.
+  * This one is the 'main method,' a special method that is run automatically when you run this
+  * program.
+  */
+  public static void main(String[] args) {
+    /*
+    * This is a method call. It invokes a method to run it. In our case, 'System.out.println()' 
+    * is a method that prints its parameter to the screen.
+    */
+    System.out.println("Hello, world!");
+  }
+}
